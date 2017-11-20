cd2dfd7a5c2d67248676e3bd09bb25c11bc00c6d
dtstrange <dustintstrange@gmail.com>
2017-11-19 17:53:38 -0800

    added dynamic content creation along with momentjs for time tables

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     //initilize firebase database
-    console.log("init")
     var config = {
         apiKey: "AIzaSyDJb6OMREhZ_xH1D2jIfk9DXvgKEbKkRg0",
         authDomain: "train-scheduler-68b03.firebaseapp.com",
@@ -16,15 +15,13 @@ $(document).ready(function() {
 
     //button to add trainschedule
     $("#add-train-btn").on("click", function(event) {
-    	alert("EYYY");
         event.preventDefault();
-        console.log("train click")
-        console.log(this)
+
 
         //capture user input
         var trainName = $("#train-name-input").val().trim();
         var destination = $("#destination-input").val().trim();
-        var startTime = $("#start-input").val().trim();
+        var startTime = moment($("#start-input").val().trim(), "HH:mm").format("X");
         var frequency = $("#frequency-input").val().trim();
 
         var trainData = {
@@ -52,11 +49,47 @@ $(document).ready(function() {
 
     }); //end on click
 
-    database.ref().on("child_added", function(snapshot) {
+    database.ref("/trains").on("child_added", function(snapshot) {
 
         console.log(snapshot.val());
 
+        //train info
+        var dbTrainName = snapshot.val().trainName;
+        var dbDestination = snapshot.val().destination;
+        var dbStartTime = snapshot.val().startTime;
+        var dbFrequency = snapshot.val().frequency;
+
+        var timeArr = dbStartTime.split(":");
+        var trainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
+        var maxMoment = moment().max(moment(), trainTime);
+        var nextTime;
+        var minAway;
+        //alert(minAway)
+        //military time diplay
+        var MT = moment.unix(dbStartTime).format("HH:mm");
+
+        if (maxMoment === trainTime) {
+            nextTime = trainTime.format("hh:mm");
+            nextTime = trainTime.diff(moment(), "minutes");
+        } else {
+            var differenceTimes = moment().diff(trainTime, "minutes");
+            var tRemainder = differenceTimes % dbFrequency;
+            tMinutes = dbFrequency - tRemainder;
+
+            tArrival = moment().add(tMinutes, "m").format("hh:mm");
+        }
 
 
-    });
-});
+        console.log(dbTrainName);
+        console.log(dbDestination);
+        console.log(dbStartTime);
+        console.log(dbFrequency)
+        console.log(MT)
+        console.log(tMinutes)
+        console.log(tArrival)
+
+        $("#tTable").append("<tr><td>" + dbTrainName + "</td><td>" + dbDestination + "</td><td>" + dbFrequency + "</td><td>" + tArrival + "</td><td>" + tMinutes + "</td></tr>")
+
+
+    }); //end on child_added
+}); //end document.ready
